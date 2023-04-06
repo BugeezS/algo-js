48daa01e9b7cb45988bb75c99cac7cfb085d8ec8
Bruno <brunolb.lafont@gmail.com>
2023-04-06 12:27:18 +0200

    add 2.5

---
+++ 2.5/2.5.js
@@ -0,0 +1,9 @@
+let favorite = prompt("What's your favorite number")
+
+while(favorite != 42){
+    alert("Are you sure ? ");
+    favorite = prompt("What's your favorite number");
+}
+if(favorite = 42){
+    alert("NICE");
+}
