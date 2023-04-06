2d98560cd9efb6eeaa57192bb7e22d32eeb09125
Bruno <brunolb.lafont@gmail.com>
2023-04-06 14:55:54 +0200

    add 3.0

---
+++ 3.0/3.0.js
@@ -0,0 +1,4 @@
+let liste = ["Abel", "Alexandra","Anthony","Benjamin","Bruno","Delphine","Emilien","Ethan","Ismaël","Jodie","Julien","Louka","Loïc","Luciano","Marine","Nikko","Noa","Steve","Sylvain","Willy"]
+for (let elem of liste){
+    console.log(elem);
+}
