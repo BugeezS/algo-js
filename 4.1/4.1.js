9130c21707817f4bfcb30c4c4c1efacd0ae04a90
Bruno <brunolb.lafont@gmail.com>
2023-04-06 16:55:04 +0200

    add 4.2

--- 4.1/4.1.js
+++ 4.1/4.1.js
@@ -3,6 +3,11 @@ let length = Number (prompt("Length"));
 
 alert(calcsurface(width,length));
 
+// Multiply 2 numbers together
+// @param {number} a The first number
+// @param {number} b The second number
+// @return {number} The total of the multiplication
+
 function calcsurface(a,b){
    return a*b;
 }
