58a60e4ea63c0ad207a09d15c6c5ad2824248fa8
Bruno <brunolb.lafont@gmail.com>
2023-04-12 09:37:02 +0200

    add 5.1

---
+++ 5.1/5.1.js
@@ -0,0 +1,26 @@
+let tv = {
+    name:"",
+    product:"",
+    cast:[],
+}
+
+
+function asktvSerie() {
+    let tv = {
+        name:"",
+        product:"",
+        cast:[],
+    }
+let name = prompt("Nom de la série");
+let product = prompt("Année de production");
+    while(cast){
+        let cast = prompt('Name cast member');
+    }
+    return {
+        name,
+        product,
+        cast,
+    }
+}
+asktvSerie();
+console.log(tv);
